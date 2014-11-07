81bf2032416f7d4b0dd4ba5c178bf830859f05c7
Keisuke Ohta <keisuke.ohta@gmail.com>
2014-11-07 23:46:36 +0900

    Initial Commit

---
+++ sekuharaowata/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sekuharaowata
+//
+//  Created by Keisuke Ohta on 2014/11/07.
+//  Copyright (c) 2014年 Keisuke Ohta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
